49d5791a4110bf09e8b24da5fb77162c34c39fac
Amy Yeung <amyyeung17@gmail.com>
2023-12-08 17:56:57 -0500

    Renamed actually

---
+++ components/navbar-og.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+  const linkStyle = 'hover:opacity-80 font-medium text-slate-700 px-2 py-1'
+  return(
+    <>
+      <div className="border-b-2 border-zinc-100 w-full overflow-x-hidden">
+        <div className="container mx-auto px-3 py-2">
+          <div className="flex gap-x-4 max-sm:justify-center justify-end max-sm:space-x-2 w-full">
+            <Link className={linkStyle} href="/"> Home </Link>
+            <Link className={linkStyle} href="/search"> Search </Link>
+            <Link className={linkStyle} href="/info"> Info </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
