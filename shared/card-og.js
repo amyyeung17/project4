e1c922cbb1d85274eb313b3c9aadd6bdc3ba85da
Amy Yeung <amyyeung17@gmail.com>
2024-10-17 13:00:01 -0400

    Remove unused files

--- shared/card-og.js
+++ shared/card-og.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import ButtonProfile from '@/shared/button-profile'
+import Link from 'next/link'
 import CardInfo from '@/shared/card-info'
 import IconCheck from '@/shared/icon-check'
 import ImageProfile from '@/shared/image-profile'
 
 const Card = ({cardStyle = 'card', info, width}) => {
   const itemStyle = {card: 'card max-phone:flex', lang: true, link: 'self-end text-sm max-sm:text-sm', langStyle: 'text-sm', info: 'grow w-full', name: 'text-lg', native: 'text-sm'}
-
+  const path= (typeof(info.type) !== 'undefined' ? 'shows' : 'actors')
   return(
     <>
-      <ButtonProfile path={typeof(info.type) !== 'undefined' ? 'shows' : 'actors'} id={info.id} buttonStyle={`${cardStyle} shadow-lg hover:shadow-2xl h-full relative bg-white w-full`}>
+       <Link href={`/${path}?id=${info.id}`} className={`${cardStyle} shadow-lg hover:shadow-2xl h-full relative bg-white w-full`}> 
         <ImageProfile siteUrl={info.image.large} width={width} />
         <CardInfo itemStyle={itemStyle} person={info} />
         {info.picked && 
           <IconCheck />
         }
-      </ButtonProfile>
+      </Link>
     </>
   )
 }
